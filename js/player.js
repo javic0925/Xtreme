cca174d04067ecf00721d421061325bd26883432
Javier Cruz <javic0925@gmail.com>
2022-10-04 12:06:26 -0400

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,46 @@
+$(function () {
+    var song = document.getElementsByTagName('audio')[0],
+        sourceMp3 = document.getElementsByTagName('audio')[0];
+
+    sourceMp3.src = 'https://c17.radioboss.fm:8162/stream';
+
+
+    $('#player').click(function (e) {
+        e.preventDefault();
+        if (song.paused) song.play();
+        else 
+        song.setAttribute('src' , 'https://c17.radioboss.fm:8162/stream'); 
+    
+    });
+    $('#player').bind('click', function() {
+        if ($('#playback').attr('src') == './assets/Icons/Play.svg')
+            $('#playback').attr('src', './assets/Icons/Pause.svg');
+        else
+            $('#playback').attr('src', './assets/Icons/Play.svg');
+    });
+
+    song.addEventListener('pause', function () {
+        song.setAttribute('src' , 'https://c17.radioboss.fm:8162/stream');
+        song.currentTime = 0;
+        $('#playback').attr('class', 'fa fa-play fa-lg');
+    });
+
+    
+
+    
+
+
+
+    
+});
+
+
+let mediaSession = navigator.mediaSession;
+
+if ("mediaSession" in navigator){
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: "Radio",
+      artist: "XtremeVibes",
+      album: "Otras Vibras",
+      artwork: [{src: "#"}]
+    });}
